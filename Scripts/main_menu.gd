7ae696e71c0384792ad1d5f742fad6d4bea326de
H4rb1ngs <jringlaben@protonmail.com>
2026-06-05 16:42:39 -0500

    Skeleton stuff
    Just added minor button stuff so its ready to be filled in

---
+++ Scripts/main_menu.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_startbutt_button_down() -> void: #middle button to either start game/pick level
+	pass
