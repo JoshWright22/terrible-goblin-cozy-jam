429bada5069880b9ca806f679d920502211fa62a
H4rb1ngs <jringlaben@protonmail.com>
2026-06-05 03:21:25 -0500

    Cleaned skeleton/added state machine
    Minor QOL clearin ups
    added state maching using currentState
    changeScene() and a preloaded node
    created main menu screen to be loaded from 'main'
    renamed 'main' to 'host' to avoid confusion

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,36 +1,32 @@
 extends Node2D
 
-var select = -1
+var currentScene #setter variable for changeScene()
 
-var menus : Array = [ #simple list of menus FROM the main title card, honestly these are stand-ins
-	"Main",
-	"HighScore",
-	"Help",
-]
-
-func _input(event: InputEvent) -> void: #Controls mouse click on button 
-	if event is InputEventMouseButton:
-		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
-			pass
+#scenes
+@onready var mainMenu = load("res://Scenes/main_menu.tscn")
 
 func _ready() -> void:
-	pass 
-
-
+	changeScene(mainMenu) 
 func _process(delta: float) -> void:
 	pass
 
-func changeScene():
-	pass
+
+func changeScene(scene) -> void:  #changes scene with input 'scene' based off of #scenes
+	currentScene = scene
+	var instance
+	if get_child_count() >= 1:
+		get_child(0).queue_free()
+	instance = currentScene.instantiate()
+	add_child(instance)
 
 
-func _on_start_butt_focus_entered() -> void:
-	pass # Replace with function body.
+func _on_start_butt_focus_entered() -> void: #Hover over startButt
+	pass 
 
 
-func _on_start_butt_focus_exited() -> void:
-	pass # Replace with function body.
+func _on_start_butt_focus_exited() -> void: #Exit startButt area
+	pass 
 
 
-func _on_start_butt_pressed() -> void:
-	pass # Replace with function body.
+func _on_start_butt_pressed() -> void: #start game
+	pass 
