53a2922e2a1f02012cba2328a20b4ded9ac3d650
H4rb1ngs <jringlaben@protonmail.com>
2026-06-06 12:35:28 -0500

    added difficulty scaling

--- Scripts/order_health_control.gd
+++ Scripts/order_health_control.gd
@@ -7,10 +7,20 @@ extends Node2D
 
 #Timer/health variables
 var MAX_TIME = 30
+var MAX_ADD_TIME = 15
+
+#Customer variables
+var EASY_MIN = 2 #NO OF ITEMS USED PER ORDER PER DIFFICULTY
+var EASY_MAX = 3
+var MED_MIN = 3
+var MED_MAX = 4
+var HARD_MIN = 3
+var HARD_MAX = 5
 
 var customerNo #tracks how many customers you've served for difficulty scaling
 
 var ingredients : Array = [ #list of things a customer may want
+"Banana", "Apple", "Cherry", "Mango", "Strawberry"
 ]
 
 var currentOrder : Dictionary = { #what customer wants in what proportions
@@ -26,5 +36,10 @@ func _process(delta: float) -> void:
 func genCustomer(): #creates customer and order
 	customerNo = customerNo + 1
 
-func genOrder():
-	pass
+func genOrder(): #creates the order and proportions of each needed; controls order difficulty
+	if customerNo <= 4: #Easy
+		pass
+	elif customerNo >= 5 && customerNo <= 8: #Med (More items, stranger proportions)
+		pass
+	else: #Hard (Most items, wacky proportions
+		pass
