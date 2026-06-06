3aabb4830e1dad5d6b6a63797d05ad9ce68fa66e
H4rb1ngs <jringlaben@protonmail.com>
2026-06-06 13:48:26 -0500

    Added health & time variables

--- Scripts/order_health_control.gd
+++ Scripts/order_health_control.gd
@@ -1,12 +1,20 @@
 extends Node2D
 @onready var healthBar = $healthbar/ProgressBar
 @onready var timer = $healthbar/Timer
+@onready var char1Sprite = $custWindow/characterSprites/Char1Sprite
+@onready var char2Sprite = $custWindow/characterSprites/Char2Sprite
+@onready var char3Sprite = $custWindow/characterSprites/Char3Sprite
+@onready var char4Sprite = $custWindow/characterSprites/Char4Sprite
+@onready var charSpawnTImer = $customerSPawner
 
 #Timer/health variables
 var MAX_TIME = 30
 var MAX_ADD_TIME = 15
 
 #Customer variables
+var MIN_CHAR_TIME = 10
+var MAX_CHAR_TIME = 15
+
 var EASY_MIN = 2 #NO OF ITEMS USED PER ORDER PER DIFFICULTY
 var EASY_MAX = 3
 var MED_MIN = 3
@@ -15,14 +23,14 @@ var HARD_MIN = 3
 var HARD_MAX = 5
 
 var customerNo #tracks how many customers you've served for difficulty scaling
-var currentCustomer
+var currentCustomerCount 
 
 var ingredients : Array = [ #list of things a customer may want
 "Banana", "Apple", "Cherry", "Mango", "Strawberry"
 ]
 
-
-var currentOrders : Array = [] #what customer wants in what proportions max 4
+var currentOrders : Array = []
+#assigned to positions left to right, not necessarily the order the customers show up
 var order1 : Dictionary = {}
 var order2 : Dictionary = {}
 var order3 : Dictionary = {}
@@ -34,6 +42,8 @@ func _process(delta: float) -> void:
 	pass
 
 func genCustomer(): #creates customer and order
+	if currentOrders.size() <= 4:
+		pass
 	customerNo = customerNo + 1
 
 func genOrder(): #creates the order and proportions of each needed; controls order difficulty
@@ -43,3 +53,8 @@ func genOrder(): #creates the order and proportions of each needed; controls ord
 		pass
 	else: #Hard (Most items, wacky proportions
 		pass
+
+
+func _on_customer_s_pawner_timeout() -> void: #next customer walks up
+	charSpawnTImer.wait_time = randi_range(MIN_CHAR_TIME, MAX_CHAR_TIME)
+	genCustomer()
